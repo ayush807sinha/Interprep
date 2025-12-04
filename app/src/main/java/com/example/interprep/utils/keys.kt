4cdffebb9b74a7938b70901f482c13f07902fc1c
ayush807sinha <112458566+ayush807sinha@users.noreply.github.com>
2025-12-04 22:39:00 +0530

    Update keys.kt

--- app/src/main/java/com/example/interprep/utils/keys.kt
+++ app/src/main/java/com/example/interprep/utils/keys.kt
@@ -1,7 +1,7 @@
 package com.example.interprep.utils
 
-val perplexityKey = ""//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
+val perplexityKey = ""
 
-val vapiKey = ""//dc94e5a3-254e-4157-b179-c1f161270662
+val vapiKey = ""
 
-val geminiApiKey = ""//AIzaSyAl1fGTr_1EwXD4rta0Z-prJiw3hrm2dy4
+val geminiApiKey = ""
